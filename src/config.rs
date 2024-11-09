a77bd221c2c3c316796abd365ff4db151196a829
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-11-09 15:55:02 +0100

    Add config and options

---
+++ src/config.rs
@@ -0,0 +1,69 @@
+use std::fs::OpenOptions;
+use std::io::{BufReader, BufWriter, Read, Write};
+use serde::{Deserialize, Serialize};
+use crate::CONFIG_FILE;
+
+#[derive(Debug)]
+pub struct Config {
+    pub hosts: Vec<String>,
+    pub targets: Vec<String>,
+    pub changed: bool,
+}
+
+#[derive(Deserialize, Serialize)]
+struct RawConfig {
+    hosts: Vec<String>,
+    targets: Vec<String>,
+}
+
+impl Config {
+    pub fn init() -> Self {
+        let file = match OpenOptions::new()
+            .read(true)
+            .open(CONFIG_FILE) {
+            Ok(f) => f,
+            Err(_) => {
+                return Config {
+                    hosts: vec![],
+                    targets: vec![],
+                    changed: true,
+                }
+            }
+        };
+            
+        let mut reader = BufReader::new(file);
+
+        let mut content = String::new();
+        reader.read_to_string(&mut content)
+              .expect(&format!("Unable to read {CONFIG_FILE}"));
+
+        let config = toml::from_str::<RawConfig>(&content)
+            .expect(&format!("Unable to parse {CONFIG_FILE}"));
+
+        Self {
+            hosts: config.hosts,
+            targets: config.targets,
+            changed: false,
+        }
+    }
+
+    pub fn dump(self) {
+        let raw = RawConfig {
+            hosts: self.hosts,
+            targets: self.targets,
+        };
+
+        let toml = toml::to_string_pretty(&raw).unwrap();
+        let file = OpenOptions::new()
+            .create(true)
+            .truncate(true)
+            .write(true)
+            .open(CONFIG_FILE)
+            .expect(&format!("Unable to open/crete/truncate {CONFIG_FILE}"));
+
+        let mut writer = BufWriter::new(file);
+
+        writeln!(writer, "{toml}")
+            .expect(&format!("Unable to write to {CONFIG_FILE}"));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+mod config;
+
+use std::io;
+use config::Config;
+
+const CONFIG_FILE: &str = "caddy_manager.toml";
+
 fn main() {
-    println!("Hello, world!");
+    let mut config = Config::init();
+    let options = format!("{} exit", get_options());
+
+    println!("Welcome to Caddy Manager");
+
+    loop {
+        println!("{options}");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim() {
+            "exit" => break,
+            _ => println!(r#"Invalid input, "exit" to exit"#),
+        }
+    }
+
+    if config.changed { config.dump(); }
+}
+
+fn get_options() -> String {
+    vec![
+        "Add",
+        "Remove",
+        "Show",
+        "Enable",
+        "Disable",
+        "Configure",
+    ].into_iter()
+        .map(|s| {
+            let mut chars = s.chars();
+            let first = chars.next().unwrap();
+            let remaining = chars.as_str();
+            format!("[{first}]{remaining}")
+        })
+        .fold(String::from("Options:"), |acc, s| format!("{acc} {s}"))
 }
